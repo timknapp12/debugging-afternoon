8afb6679743a13f33436d10c98e9c87dfbe40caf
timknapp12 <timknapp12@gmail.com>
2017-10-24 15:40:42 -0600

    first commit

--- stage-4/src/index.js
+++ stage-4/src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-
+<HashRouter>
     <Provider store={store}>
         <App />
     </Provider>
+</HashRouter>
     , document.getElementById('root'));
 registerServiceWorker();
